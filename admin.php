4ef0ee67f7e338cff7d7546c1df944d463965971
Shafayat1004 <shafayat1004@live.com>
2022-03-30 15:03:43 +0600

    final commit

--- admin.php
+++ admin.php
@@ -29,6 +29,24 @@ mysqli_close($link);
     <link rel="stylesheet" href="static/css/my.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">1910456 Assignment 2</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="admin.php">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact-form.html">Contact Form</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
     <table class="table">
     <thead>
         <tr>
--- contact.php
+++ contact.php
@@ -9,7 +9,24 @@
     <link rel="stylesheet" href="static/css/my.css">
 </head>
 <body>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">1910456 Assignment 2</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin.php">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="contact-form.html">Contact Form</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
     <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
